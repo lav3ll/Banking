56dba7f2f27bd4e607fe620200ca7f640cd14440
lav3ll <lavell.francis@hotmail.com>
2023-03-14 20:21:36 +0000

    final functionality changes to website

--- script.js
+++ script.js
@@ -10,6 +10,18 @@ const account1 = {
   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
   interestRate: 1.2, // %
   pin: 1111,
+  movementsDates: [
+    '2022-11-06T21:31:17.178Z',
+    '2022-12-07T07:42:02.383Z',
+    '2023-01-08T09:15:04.904Z',
+    '2023-02-09T10:17:24.185Z',
+    '2023-02-10T14:11:59.604Z',
+    '2023-02-11T17:01:17.194Z',
+    '2023-02-12T23:36:17.929Z',
+    '2023-02-13T10:51:36.790Z',
+  ],
+  currency: 'GBP',
+  locale: 'en-UK', // de-DE
 };
 
 const account2 = {
@@ -17,6 +29,18 @@ const account2 = {
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: 2222,
+  movementsDates: [
+    '2022-11-01T13:15:33.035Z',
+    '2022-11-30T09:48:16.867Z',
+    '2022-12-25T06:04:23.907Z',
+    '2023-01-25T14:18:46.235Z',
+    '2023-02-05T16:33:06.386Z',
+    '2023-04-10T14:43:26.374Z',
+    '2023-06-25T18:49:59.371Z',
+    '2023-07-26T12:01:20.894Z',
+  ],
+  currency: 'USD',
+  locale: 'en-US',
 };
 
 const account3 = {
@@ -24,6 +48,18 @@ const account3 = {
   movements: [200, -200, 340, -300, -20, 50, 400, -460],
   interestRate: 0.7,
   pin: 3333,
+  movementsDates: [
+    '2022-11-01T13:15:33.035Z',
+    '2022-11-30T09:48:16.867Z',
+    '2022-12-25T06:04:23.907Z',
+    '2023-01-25T14:18:46.235Z',
+    '2023-02-05T16:33:06.386Z',
+    '2023-04-10T14:43:26.374Z',
+    '2023-06-25T18:49:59.371Z',
+    '2023-07-26T12:01:20.894Z',
+  ],
+  currency: 'EUR',
+  locale: 'en-GB',
 };
 
 const account4 = {
@@ -31,6 +67,18 @@ const account4 = {
   movements: [430, 1000, 700, 50, 90],
   interestRate: 1,
   pin: 4444,
+  movementsDates: [
+    '2019-11-01T13:15:33.035Z',
+    '2019-11-30T09:48:16.867Z',
+    '2019-12-25T06:04:23.907Z',
+    '2020-01-25T14:18:46.235Z',
+    '2020-02-05T16:33:06.386Z',
+    '2020-04-10T14:43:26.374Z',
+    '2020-06-25T18:49:59.371Z',
+    '2020-07-26T12:01:20.894Z',
+  ],
+  currency: 'GBP',
+  locale: 'en-GB',
 };
 
 const accounts = [account1, account2, account3, account4];
@@ -61,21 +109,65 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
-const account = accounts.find(acc => acc.owner === 'Jessica Davis');
+///////////////////////////////////////////////// Functions
 
-const displayMovements = function (movements, sort = false) {
+const formatMovementDate = function (date, locale) {
+  const calcDaysPassed = (date1, date2) =>
+    Math.round(Math.abs(date2 - date1) / (1000 * 60 * 60 * 24));
+
+  const daysPassed = calcDaysPassed(new Date(), date);
+  console.log(daysPassed);
+
+  if (daysPassed === 0) return 'Today';
+  if (daysPassed === 1) return 'Yesterday';
+  if (daysPassed <= 7) return `${daysPassed} days ago`;
+
+  return new Intl.DateTimeFormat(locale).format(date);
+};
+
+const options2 = {
+  style: 'currency',
+  unit: 'percent',
+  currency: 'EUR',
+  // useGrouping: false,
+};
+
+const num = 2424242;
+console.log('US:', new Intl.NumberFormat('en-US', options2).format(num));
+console.log('Germany:', new Intl.NumberFormat('de-DE', options2).format(num));
+console.log('Syria:', new Intl.NumberFormat('ar-SY', options2).format(num));
+console.log(
+  'Browser:',
+  new Intl.NumberFormat(navigator.language, options2).format(num)
+);
+
+const formatCur = function (value, locale, currency) {
+  return new Intl.NumberFormat(locale, {
+    style: 'currency',
+    currency: currency,
+  }).format(value);
+};
+
+const displayMovements = function (acc, sort = false) {
   containerMovements.innerHTML = ``;
 
-  const movs = sort ? movements.slice().sort((a, b) => a - b) : movements;
+  const movs = sort
+    ? acc.movements.slice().sort((a, b) => a - b)
+    : acc.movements;
 
   movs.forEach((mov, i) => {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
 
+    const date = new Date(acc.movementsDates[i]);
+    const dispalyDate = formatMovementDate(date, acc.locale);
+
+    const formattedMov = formatCur(mov, acc.locale, acc.currency);
+
     const html = `
     <div class="movements__row">
     <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
-    <div class="movements__date">3 days ago</div>
-    <div class="movements__value">${mov}£</div>
+    <div class="movements__date">${dispalyDate}</div>
+    <div class="movements__value">${formattedMov}</div>
   </div>
   `;
     containerMovements.insertAdjacentHTML('afterbegin', html);
@@ -83,32 +175,36 @@ const displayMovements = function (movements, sort = false) {
   const euroToUsd = 1.1;
   const poundToUsd = 1.21;
 
-  const totalDepositsInUSD = movements
+  const totalDepositsInUSD = acc.movements
     .filter(mov => mov > 0)
     .map(mov => mov * euroToUsd)
     .reduce((acc, mov) => acc + mov, 0);
 
-  const movementsUSD = movements.map(mov => mov * euroToUsd);
+  const movementsUSD = acc.movements.map(mov => mov * euroToUsd);
 
   const movementsUSDfor = [];
-  for (const mov of movements) movementsUSDfor.push(mov * euroToUsd);
+  for (const mov of acc.movements) movementsUSDfor.push(mov * euroToUsd);
 };
 
 const calcDisplayBalance = function (acc) {
   acc.balance = acc.movements.reduce((acc, mov) => acc + mov, 0);
-  labelBalance.textContent = `£${acc.balance}`;
+  labelBalance.textContent = formatCur(acc.balance, acc.locale, acc.currency);
 };
 
 const calcDisplaySummary = function (acc) {
   const incomes = acc.movements
     .filter(mov => mov > 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumIn.textContent = `£${incomes}`;
+  labelSumIn.textContent = formatCur(incomes, acc.locale, acc.currency);
 
   const outgoings = acc.movements
     .filter(mov => mov < 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumOut.textContent = `£${Math.abs(outgoings)}`;
+  labelSumOut.textContent = formatCur(
+    Math.abs(outgoings),
+    acc.locale,
+    acc.currency
+  );
 
   const interest = acc.movements
     .filter(mov => mov > 0)
@@ -117,7 +213,11 @@ const calcDisplaySummary = function (acc) {
       return int >= 1;
     })
     .reduce((acc, int) => acc + int, 0);
-  labelSumInterest.textContent = `£${acc.interestRate}`;
+  labelSumInterest.textContent = formatCur(
+    acc.interestRate,
+    acc.locale,
+    acc.currency
+  );
 };
 
 const createUsernames = function (accs) {
@@ -134,7 +234,7 @@ createUsernames(accounts);
 
 const updateUI = function (acc) {
   //Dispay Movements
-  displayMovements(acc.movements);
+  displayMovements(acc);
 
   //Display Balance
   calcDisplayBalance(acc);
@@ -143,8 +243,34 @@ const updateUI = function (acc) {
   calcDisplaySummary(acc);
 };
 
+const startLogOutTimer = function () {
+  const tick = function () {
+    const min = String(Math.trunc(time / 60)).padStart(2, 0);
+    const sec = String(time % 60).padStart(2, 0);
+    //In each call, print the remaining time to the user interface
+    labelTimer.textContent = `${min}:${sec}`;
+
+    //When we reach 0 seconds, stop timer and log out user
+    if (time == 0) {
+      clearInterval(timer);
+      labelWelcome.textContent = 'Log in to get started';
+      containerApp.style.opacity = 0;
+    }
+
+    //Decrease time
+    time--;
+  };
+  //Set time to 5 Minutes
+  let time = 300;
+  tick();
+  const timer = setInterval(tick, 1000);
+  return timer;
+};
 ////////////////Event Handlers///////////////////////
-let currentAccount = '';
+let currentAccount, timer;
+
+// const testNow = new Date();
+// labelDate.textContent = new Intl.DateTimeFormat('en-GB').format(testNow);
 
 btnLogin.addEventListener('click', function (e) {
   // Prevent form from submiting
@@ -161,15 +287,42 @@ btnLogin.addEventListener('click', function (e) {
     labelWelcome.textContent = `Welcome back, ${
       currentAccount.owner.split(' ')[0]
     }`;
+
     containerApp.style.opacity = 100;
+
+    // //Create current date and time
+    // const now = new Date();
+    const now = new Date();
+    const options = {
+      hour: 'numeric',
+      minute: 'numeric',
+      day: 'numeric',
+      month: 'numeric',
+      year: 'numeric',
+      weekday: 'long',
+    };
+
+    labelDate.textContent = new Intl.DateTimeFormat(
+      currentAccount.locale,
+      options
+    ).format(now);
 
     //Clear the input field
     inputLoginPin.value = inputLoginUsername.value = '';
     inputLoginPin.blur();
 
+    //Timer
+    if (timer) clearInterval(timer);
+
+    timer = startLogOutTimer();
+
     updateUI(currentAccount);
   } else {
     alert('Usermame or password is incorrect');
+
+    //Reset Timer
+    clearInterval(timer);
+    timer = startLogOutTimer();
   }
 });
 
@@ -191,8 +344,14 @@ btnTransfer.addEventListener('click', function (e) {
     currentAccount.movements.push(-amount);
     receiverAcc.movements.push(amount);
 
+    //Add transfer Date
+    currentAccount.movementsDates.push(new Date().toISOString());
+    receiverAcc.movementsDates.push(new Date().toISOString());
+
     //Update UI
     updateUI(currentAccount);
+    clearInterval(timer);
+    timer = startLogOutTimer();
   }
 });
 
@@ -201,11 +360,19 @@ btnLoan.addEventListener('click', function (e) {
 
   const amount = Number(inputLoanAmount.value);
   if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
-    //Add movement
-    currentAccount.movements.push(amount);
+    setTimeout(function () {
+      //Add movement
+      currentAccount.movements.push(amount);
 
-    //Update UI
-    updateUI(currentAccount);
+      //Add loan date
+      currentAccount.movementsDates.push(new Date().toISOString());
+
+      //Update UI
+      updateUI(currentAccount);
+      //Reset Timer
+      clearInterval(timer);
+      timer = startLogOutTimer();
+    }, 2500);
   }
   inputLoanAmount.value = '';
 });
@@ -241,3 +408,9 @@ btnSort.addEventListener('click', function (e) {
   displayMovements(currentAccount.movements, !sorted);
   sorted = !sorted;
 });
+
+//Fake Always logged in
+
+// currentAccount = account1;
+// updateUI(currentAccount);
+// containerApp.style.opacity = 100;
